428249fc3c28a413295a1ee86d3b72f4e590e0ad
toddmartin <kj4ntv@gmail.com>
2021-03-08 07:26:45 -0800

    Implement read in keys
    added in code to read in keys from a config file path fed in through a command
    line argument.

--- src/main/java/Jalgo.java
+++ src/main/java/Jalgo.java
@@ -1,5 +1,14 @@
 package Jalgo;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.File;
+
+import org.json.JSONObject;
+
+import java.util.Scanner;
+
 public class Jalgo {
     public static void main (String[] args) {
         if (args.length != 1) {
@@ -7,5 +16,24 @@ public class Jalgo {
             System.exit(1);
         }
         System.out.println("Welcome to Jago.\nInitializing......");
+        JSONObject keyJSON = new JSONObject(readFile(new File(args[0])));
+        Keys keys = new Keys(keyJSON.getString("api_key"), keyJSON.getString("secret_key"));
+
+        
+
+    }
+
+    private static String readFile (File file) {
+        try {
+            Scanner scan = new Scanner(file);
+            String fileContents = "";
+            while (scan.hasNext())
+                fileContents = fileContents + scan.nextLine();
+            return fileContents;
+        } catch (IOException e) {
+            System.err.println("Config file does not exist. Please checkw file path or create a config file for keys.");
+            System.exit(1);
+        }
+        return null;
     }
 }
