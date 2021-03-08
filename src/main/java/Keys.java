6e0ea56b263e3a606cb305c2f5f6a4900b1c762c
toddmartin <kj4ntv@gmail.com>
2021-03-08 08:21:05 -0800

    Handle url for live or paper trading

--- src/main/java/Keys.java
+++ src/main/java/Keys.java
@@ -3,10 +3,14 @@ package Jalgo;
 public class Keys {
     private String apiKey;
     private String secretKey;
+    private String liveEndPoint;
+    private String paperEndPoint;
 
-    public Keys (String initApiKey, String initSecretKey) {
+    public Keys (String initApiKey, String initSecretKey, String initLiveEnpoint, String initPaperEndPoint) {
         this.apiKey = initApiKey;
         this.secretKey = initSecretKey;
+        this.liveEndPoint = initLiveEnpoint;
+        this.paperEndPoint = initPaperEndPoint;
     }
 
     public String getAPIKey () {
@@ -16,4 +20,14 @@ public class Keys {
     public String getSecretKey () {
         return secretKey;
     }
+
+    public String getLiveEndPoint () {
+        return liveEndPoint;
+    }
+
+    public String getPaperEndpoint () {
+        return paperEndPoint;
+    }
+
+
 }
