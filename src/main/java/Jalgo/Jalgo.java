fe7c859ab6cf267b51365aa952928d1bc6298fd4
toddmartin <kj4ntv@gmail.com>
2021-05-05 20:21:27 -0700

    Provide live market feed on a single stock

--- src/main/java/Jalgo/Jalgo.java
+++ src/main/java/Jalgo/Jalgo.java
@@ -3,12 +3,21 @@ package dev.toddmartin.Jalgo;
 import dev.toddmartin.Jalgo.ui.ConfCreator;
 import dev.toddmartin.Jalgo.ui.gui.GuiConfCreator;
 import dev.toddmartin.Jalgo.ui.text.TextConfCreator;
-import net.jacobpeterson.domain.alpaca.account.Account;
+import java.time.ZonedDateTime;
+import net.jacobpeterson.abstracts.websocket.exception.WebsocketException;
 import net.jacobpeterson.alpaca.AlpacaAPI;
 import net.jacobpeterson.alpaca.rest.exception.AlpacaAPIRequestException;
 import net.jacobpeterson.alpaca.rest.exception.AlpacaAPIRequestException;
 import net.jacobpeterson.alpaca.enums.api.EndpointAPIType;
 import net.jacobpeterson.alpaca.enums.api.DataAPIType;
+import net.jacobpeterson.domain.alpaca.account.Account;
+import net.jacobpeterson.domain.alpaca.asset.Asset;
+import net.jacobpeterson.domain.alpaca.marketdata.realtime.MarketDataMessage;
+import net.jacobpeterson.domain.alpaca.marketdata.historical.quote.Quote;
+import net.jacobpeterson.alpaca.websocket.marketdata.listener.MarketDataListener;
+import net.jacobpeterson.alpaca.websocket.marketdata.listener.MarketDataListenerAdapter;
+import net.jacobpeterson.alpaca.websocket.marketdata.message.MarketDataMessageType;
+import net.jacobpeterson.domain.alpaca.marketdata.realtime.trade.TradeMessage;
 
 public class Jalgo {
 
@@ -16,8 +25,13 @@ public class Jalgo {
      * Main entry point of program.
      */
     public static void main (String[] args) {
+        Account acc = null;
         if (args.length < 1) {
             System.err.println("Need an argument for the interface type");
+            System.exit(1);
+        } else if (args.length < 2) {
+            System.err.println("Provide a ticker symbol");
+            System.exit(1);
         }
         Configuration conf = new Configuration();
         conf.checkForConfigFolder();
@@ -31,6 +45,51 @@ public class Jalgo {
                 cc.newConfFile();
             }
         }
+        AlpacaAPI api = conf.getAccount();
+        try {
+            acc = api.getAccount();
+        } catch (AlpacaAPIRequestException e) {
+            System.err.println("Issue getting account, check network connction");
+            System.exit(1); // exit program since account can not be gathered
+        }
+
+        System.out.println("Account cash: " + acc.getCash());
+        System.out.println("Portfolio value: " + acc.getPortfolioValue());
+
+        Asset asset = null;
+        try {
+            asset = api.getAssetBySymbol(args[1].toUpperCase());
+        } catch (AlpacaAPIRequestException e) {
+            
+        }
+        System.out.println(asset);
+
+        MarketDataListener listener = null;
+
+        try {
+
+            listener = new MarketDataListenerAdapter(
+                    args[1].toUpperCase(),
+                                                                            MarketDataMessageType.TRADE
+                                                         ) {
+            @Override
+                public void onStreamUpdate(MarketDataMessageType streamMessageType, MarketDataMessage streamMessage) {
+                if (streamMessageType == MarketDataMessageType.TRADE) {
+                    TradeMessage tradeMessage = (TradeMessage) streamMessage;
+                    System.out.println("Price: " + tradeMessage.getPrice());
+                }
+            }
+            };
+            api.addMarketDataStreamListener(listener);
+            Thread.sleep(100);
+        } catch (WebsocketException e) {
+            System.err.println("Error occured in creating a market listener");
+            System.exit(1);
+        } catch (InterruptedException ie) {
+
+        }
+
     }
+
 
 }
