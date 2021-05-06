b2854a6e8cc2cc98a05974f85a644ff5875f7e38
toddmartin <kj4ntv@gmail.com>
2021-05-06 11:25:33 -0700

    Initial implementation of Scalping sample stategy

--- src/main/java/Jalgo/strategy/ScalpingStrategy.java
+++ src/main/java/Jalgo/strategy/ScalpingStrategy.java
@@ -1,28 +1,42 @@
 package dev.toddmartin.Jalgo.strategy;
 
+import dev.toddmartin.Jalgo.ds.TradeList;
+
 public class ScalpingStrategy implements Strategy {
 
     private float lastBuyPrice;
     private float lastSellPrice;
+    private float lastTradePrice;
+    private TradeList history;
 
     public ScalpingStrategy () {
         lastBuyPrice = 0;
         lastSellPrice = 0;
+        history = new TradeList();
     }
 
     public DecisionEnum evaluate (float price) {
-        if (lastBuyPrice == 0) {
+        history.addNode(price);
+        if (lastBuyPrice == 0 && lastSellPrice == 0) {
             lastBuyPrice = price;
             return DecisionEnum.BUY;
         }
 
         if (price < lastBuyPrice) {
             lastSellPrice = price;
-            return DecisionEnum.BUY;
+            lastBuyPrice = 0;
+            return DecisionEnum.SELL;
         }
 
-        if (true) {
-            
+        if (price > lastBuyPrice) {
+            lastSellPrice = price;
+            lastBuyPrice = 0;
+            return DecisionEnum.SELL;
+        }
+
+        if (lastBuyPrice == 0 && price < lastSellPrice) {
+            lastBuyPrice = price;
+            return DecisionEnum.BUY;
         }
 
         return DecisionEnum.HOLD;
